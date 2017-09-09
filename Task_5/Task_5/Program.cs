d8755e91928388e46f38f7ac85daf6adc0addc8d
Екатерина <@LENOVO-PC>
2017-09-09 23:18:59 +0500

    start

---
+++ Task_5/Task_5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите размер квадратной матрицы");
+            //Random rnd = rnd.Next();
+            int N = Convert.ToInt32(Console.ReadLine());
+            if (N > 2 && N % 2 == 1)
+            {
+
+                double[] MasDouble = new double[N];
+
+
+
+            }
+
+
+
+
+
+        }
+    }
+}
