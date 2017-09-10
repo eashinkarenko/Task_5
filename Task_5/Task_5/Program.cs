de0e2b4572e4ee4a41a656334ea6477fdc068cf8
Екатерина <@LENOVO-PC>
2017-09-10 13:24:12 +0500

    Решение без проверки на ввод

--- Task_5/Task_5/Program.cs
+++ Task_5/Task_5/Program.cs
@@ -12,7 +12,11 @@ namespace Task_5
         {
             Console.WriteLine(@"Программа считает сумму элементов вернхнего треугольника квадратной матрицы
 Например: матрица 5х5:");
-            for(int i =0; i<5; i++)
+            //печать примера
+            //  х х х 
+            //  х х х 
+            //  х х х 
+            for (int i =0; i<5; i++)
             {
                 for (int j=0; j<5; j++)
                 {
@@ -41,21 +45,66 @@ namespace Task_5
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine("Введите размер квадратной матрицы");
-            Console.WriteLine("Для вычисления ");
 
+            
+            Console.Write("Введите размер квадратной матрицы : ");
+            
             Random rnd = new Random();
+            //задаётся размерность матрицы
             int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("         Матрица " + N + "x" + N);    
+            
+            // заполнение матрицы с помощбю ДСЧ      
             if (N > 2 && N % 2 == 1)
             {
 
-                double[] MasDouble = new double[N];
+                double[,] MasDouble = new double[N,N];
+                //заполнение и печать
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < N; j++)
+                    {
+                        MasDouble[i, j] = rnd.Next(-1000, 1000) + rnd.Next(-100, 100) * 0.01;
+                        Console.Write("  " + MasDouble[i, j] + "  ");
+                    }
+                    Console.WriteLine();
+                }
 
+                // подсчёт суммы нужных элементов
+                Console.WriteLine("Просуммированные элементы : ");
 
+                // границы для отслеживания нужного участка матрицы и количество просуммированных элементов
+                int Start = 0, End = N, CountSumOf = 0;
 
+                // сумма элементов, принадлежащих области
+                double CountSum = 0;
+
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < N; j++)
+                    {
+                        if ((j >= Start) && (j < End) && (i <=End))
+                        {
+                            CountSumOf++;
+                            CountSum += MasDouble[i, j];
+                            // параллельная печать суммируемых объектов
+                            Console.Write("  " + MasDouble[i, j] + "  ");
+                        }
+                    }
+                    Start++;
+                    End--;
+                    if  (i <= End)  Console.WriteLine();
+                }
+                Console.WriteLine();
+
+                // печать результатов
+                Console.WriteLine("Итог суммирования = " + CountSum + " ( " + CountSumOf + " объектов)");
             }
+            else Console.Write(@"Извините, работа с данным размером матрицы не может быть выполнена,
+так как нельзя выделить четкую область для суммирования в ней элементов.");
 
-
+            
+            Console.ReadLine();
         }
     }
 }
